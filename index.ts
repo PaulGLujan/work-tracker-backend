2a70e01d000e5218ca49a8f3cd6ab6d5370f591b
Paul <paulglujan@gmail.com>
2023-03-03 15:16:52 -0800

    Formatting code

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
-import { getAlertTimesByDuration, getNextQuarterHour } from './utils/time';
-import { Pool } from 'pg';
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import { getAlertTimesByDuration, getNextQuarterHour } from "./utils/time";
+import { Pool } from "pg";
 
 dotenv.config();
 
@@ -16,29 +16,28 @@ const pool = new Pool({
 const app: Express = express();
 const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
 });
 
-app.get("/users", (req: Request, res: Response) => {
-  pool.query("select * from users", (err, result) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send("Error retrieving users from database");
-    } else {
-      res.send(result.rows);
-    }
-  });
+app.get("/users", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query("select * from users");
+    res.send(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error retrieving users from database");
+  }
 });
 
-app.get('/alertTimes/:startTime/:duration', (req: Request, res: Response) => {
-    const { duration, startTime } = req.params;
-    const startTimeDate = new Date(Number(startTime));
-    const nextQuarterHour = getNextQuarterHour(startTimeDate);
-    const durationString = Number(duration);
-    res.send(getAlertTimesByDuration(nextQuarterHour, durationString));
+app.get("/alertTimes/:startTime/:duration", (req: Request, res: Response) => {
+  const { duration, startTime } = req.params;
+  const startTimeDate = new Date(Number(startTime));
+  const nextQuarterHour = getNextQuarterHour(startTimeDate);
+  const durationString = Number(duration);
+  res.send(getAlertTimesByDuration(nextQuarterHour, durationString));
 });
 
 app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
