068c648c36be8bcca819d510b4cecdaafa9241fd
Paul <paulglujan@gmail.com>
2023-03-03 15:00:48 -0800

    Connected to DB and got user table

--- index.ts
+++ index.ts
@@ -1,14 +1,34 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import { getAlertTimesByDuration, getNextQuarterHour } from './utils/time';
+import { Pool } from 'pg';
 
 dotenv.config();
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: parseInt(process.env.DB_PORT ?? "5432"),
+});
 
 const app: Express = express();
 const port = process.env.PORT;
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
+});
+
+app.get("/users", (req: Request, res: Response) => {
+  pool.query("select * from users", (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Error retrieving users from database");
+    } else {
+      res.send(result.rows);
+    }
+  });
 });
 
 app.get('/alertTimes/:startTime/:duration', (req: Request, res: Response) => {
